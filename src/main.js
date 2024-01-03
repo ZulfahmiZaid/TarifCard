b455876fc35a1a8be3b9cdb1691cb659a718d1cc
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2024-01-03 10:49:38 +0100

    feat: installed Vuetify and mdi-fonts

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createVuetify} from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css';
 
-createApp(App).mount('#app')
+import {
+    VCard
+} from "vuetify/components";
+
+const app = createApp(App)
+
+// create Vuetify
+const vuetify = new createVuetify({
+    components:{
+        VCard
+    }
+})
+
+// use Vuetify
+app.use(vuetify)
+
+// Mount app
+app.mount('#app')
+
+
